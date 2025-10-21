f6efe776176aad7fe2d76a1e364f8f8ccbdf71bd
Christian Louis Bolea <bolea2133@gmail.com>
2025-10-21 20:03:44 +0800

    3

---
+++ NameCalculator/app.js
@@ -0,0 +1,23 @@
+(function() {
+    'use strict';
+    angular
+        .module('NameCalculator', [])
+
+        .controller('NameCalculatorController', ['$scope', function($scope) {
+            $scope.name = "jose";
+            $scope.totalValue = 0;
+
+            $scope.displayNumeric = function() {
+                $scope.totalValue = calculateNumericForString($scope.name);
+            };
+
+            function calculateNumericForString(string) {
+                var totalStringValue = 0;
+                for (var i = 0; i < string.length; i++) {
+                    totalStringValue += string.charCodeAt(i);
+                }
+                return totalStringValue;
+            }
+        }]);
+
+})();
